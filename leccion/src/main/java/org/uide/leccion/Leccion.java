be299b6f0bd905ef953b9856260ccfe133c17904
Kevin Suarez <kesuarezar@uide.edu.ec>
2023-05-19 07:30:54 -0500

    main

---
+++ leccion/src/main/java/org/uide/leccion/Leccion.java
@@ -0,0 +1,10 @@
+
+package org.uide.leccion;
+
+public class Leccion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Persona persona = new Persona();
+    }
+}
---
+++ leccion/src/main/java/org/uide/leccion/Persona.java
@@ -0,0 +1,9 @@
+package org.uide.leccion;
+
+public class Persona {
+    public Persona() {
+        System.out.println("Persona creada");
+    }
+
+    
+}
