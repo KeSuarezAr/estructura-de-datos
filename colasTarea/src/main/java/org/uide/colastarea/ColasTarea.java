4ca95ecb9db5213c18e400abf96d93c571bf7420
Kevin Suarez <kesuarezar@uide.edu.ec>
2023-06-09 18:50:01 -0500

    main

--- colasTarea/src/main/java/org/uide/colastarea/ColasTarea.java
+++ colasTarea/src/main/java/org/uide/colastarea/ColasTarea.java
@@ -6,9 +6,6 @@ public class ColasTarea {
     public static void main(String[] args) {
         GestorBanco gc = new GestorBanco();
 
-        // gc.llegadaCliente(new Cliente("Kevin", "Andres", "1100444809", "0959403201",
-        // "Test"));
-        // gc.atencionCliente();
-        gc.simulacion(5000);
+        gc.simulacion(500);
     }
 }
