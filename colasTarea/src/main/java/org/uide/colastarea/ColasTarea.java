823d9b4df5cd3e85cb83cc4fd400eba119b0d2ff
Kevin Suarez <kesuarezar@uide.edu.ec>
2023-06-09 09:44:35 -0500

    main

---
+++ colasTarea/src/main/java/org/uide/colastarea/ColasTarea.java
@@ -0,0 +1,9 @@
+
+package org.uide.colastarea;
+
+public class ColasTarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ colasTarea/src/main/java/org/uide/colastarea/GestorBanco.java
@@ -0,0 +1,13 @@
+package org.uide.colastarea;
+
+public class GestorBanco {
+
+    public GestorBanco() {
+    }
+
+    public void listarClientes() {
+    }
+
+    public void agregarCliente(Cliente cliente) {
+    }
+}
