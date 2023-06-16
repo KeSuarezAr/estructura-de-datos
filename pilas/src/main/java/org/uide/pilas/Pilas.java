d889e09b9e713e41ffd7d5ab1dd2c7e4ba812335
Kevin Suarez <kesuarezar@uide.edu.ec>
2023-06-16 10:04:28 -0500

    main

--- pilas/src/main/java/org/uide/pilas/Pilas.java
+++ pilas/src/main/java/org/uide/pilas/Pilas.java
@@ -1,16 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package org.uide.pilas;
 
-/**
- *
- * @author MsiComputer
- */
+import java.util.Scanner;
+
 public class Pilas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        GestorHistorial gh = new GestorHistorial();
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Ingrese la url: ");
+            String url = sc.nextLine();
+            System.out.println("Ingrese el titulo: ");
+            String titulo = sc.nextLine();
+            System.out.println("Ingrese el dispositivo: ");
+            String dispositivo = sc.nextLine();
+            System.out.println("Ingrese la fecha de cierre: ");
+            String fechaCierre = sc.nextLine();
+            gh.visitarWeb(new PaginaWeb(url, titulo, dispositivo, fechaCierre));
+        }
+
+        PaginaWeb movil = gh.obtenerUltimoDispositivo();
+
+        System.out.println(movil);
+
+        sc.close();
     }
 }
